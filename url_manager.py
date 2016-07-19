dde346f64f01306c9e0bd91ddddb6045e80785b6
yaochao <yaochao365@qq.com>
2016-07-19 14:19:21 +0800

    url_manager functions complete

--- url_manager.py
+++ url_manager.py
@@ -2,14 +2,26 @@
 
 class UrlManager(object):
 
+    def __init__(self):
+        self.new_urls = set()
+        self.old_urls = set()
+
     def add_new_url(self, new_url):
-        pass
+        if new_url is None:
+            return
+        if new_url not in self.new_urls and new_url not in self.old_urls:
+            self.new_urls.add(new_url)
 
     def add_new_urls(self, new_urls):
-        pass
+        if new_urls is None or len(new_urls) == 0:
+            return
+        for new_url in new_urls:
+            self.add_new_url(new_url)
 
     def has_new_url(self):
-        pass
+        return len(self.new_urls) != 0
 
     def get_new_url(self):
-        pass
+        new_url = self.new_urls.pop()
+        self.old_urls.add(new_url)
+        return new_url
