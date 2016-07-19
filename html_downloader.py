ceeeb5d24da09b2e67ef1b1331a8dab1cf0c88bf
yaochao <yaochao365@qq.com>
2016-07-19 14:46:00 +0800

    html_downloader get_content complete

--- html_downloader.py
+++ html_downloader.py
@@ -1,6 +1,14 @@
 # encoding: utf-8
 
+import requests
+
 class HtmlDownloader(object):
 
     def get_content(self, url):
-        pass
+        if url is None:
+            return
+        response = requests.get(url)
+        if response.status_code != 200:
+            return
+        return response.content
+    
