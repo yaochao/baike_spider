cda42f92f2d2d1b91e87d1366e5e3aa3d815f728
yaochao <yaochao365@qq.com>
2016-07-19 15:30:39 +0800

    html_parser functions complete

--- html_parser.py
+++ html_parser.py
@@ -1,6 +1,39 @@
 # encoding: utf-8
 
+from bs4 import BeautifulSoup
+import re
+import urlparse
+
 class HtmlParser(object):
 
+    def _get_new_urls(self, url, soup):
+        new_full_urls = set()
+        a_nodes = soup.find_all('a', href=re.compile(r'/view/\d+\.htm'))
+        for a_node in a_nodes:
+            new_url = a_node['href']
+            new_full_url = urlparse.urljoin(url, new_url)
+            new_full_urls.add(new_full_url)
+        return new_full_urls
+
+    def _get_new_data(self, url, soup):
+        new_data = {}
+        new_data['url'] = url
+        title_node = soup.find('dd', class_='lemmaWgt-lemmaTitle-title').find('h1')
+        title = title_node.get_text()
+        new_data['title'] = title
+        summary_node = soup.find('div', class_='lemma-summary')
+        summary = summary_node.get_text()
+        new_data['summary'] = summary
+        return new_data
+
+
+
+        return new_data
+
     def parse(self, url, content):
-        pass
+        if url is None or content is None:
+            return
+        soup = BeautifulSoup(content, 'html.parser', form_encoding='utf-8')
+        new_urls = self._get_new_urls(url, soup)
+        new_data = self._get_new_data(url, soup)
+        return new_urls, new_data
